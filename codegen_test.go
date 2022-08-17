8695f7d0c3e376707a66cb47b96469ed64288c33
Adam Bull <adam@keel.xyz>
2022-08-17 19:55:00 +0100

    add basic test

--- codegen_test.go
+++ codegen_test.go
@@ -5,39 +5,64 @@ import (
 	"testing"
 
 	generator "github.com/adaam2/ts-generator-go"
+	"gotest.tools/v3/assert"
 )
 
 func TestGenerator(t *testing.T) {
 	tmpDir := os.TempDir()
-	cg := generator.NewGenerator(tmpDir, &generator.GeneratorOptions{
-		Indent: 2,
-	})
 
-	sf := cg.AddSourceFile("test.ts")
-
-	sf.AddInterface("TestInterface", true, func(i *generator.Interface) {
-		i.AddProperty("testProp", "string")
-		i.AddProperty("anotherProp", "number")
-	})
-
-	sf.AddClass("MyClass", true, func(c *generator.Class) {
-		c.AddMember("something", "string", "private")
-
-		c.AddConstructor(func(cb *generator.Constructor) {
-			cb.AddParameter("something", "string")
-			cb.AddAssignment("this.something", "something")
+	t.Run("round trip", func(t *testing.T) {
+		cg := generator.NewGenerator(tmpDir, &generator.GeneratorOptions{
+			Indent: 2,
 		})
 
-		c.AddClassMethod("doSomething", func(method *generator.ClassMethod) {
-			method.SetReturnType("string")
-			method.SetScope("private")
-			method.AddParameter("input", "string")
-			method.AddParameter("anotherInput", "number")
+		sf := cg.AddSourceFile("test.ts")
+
+		sf.AddInterface("TestInterface", true, func(i *generator.Interface) {
+			i.AddProperty("testProp", "string")
+			i.AddProperty("anotherProp", "number")
 		})
+
+		sf.AddClass("MyClass", true, func(c *generator.Class) {
+			c.AddMember("something", "string", "private")
+
+			c.AddConstructor(func(cb *generator.Constructor) {
+				cb.AddParameter("something", "string")
+				cb.AddAssignment("this.something", "something")
+			})
+
+			c.AddClassMethod("doSomething", func(method *generator.ClassMethod) {
+				method.SetReturnType("string")
+				method.SetScope("private")
+				method.AddParameter("input", "string")
+				method.AddParameter("anotherInput", "number")
+			})
+		})
+
+		out := cg.String()
+
+		expected := `
+// test.ts
+export interface TestInterface {
+  testProp: string;
+  anotherProp: number;
+}
+
+export class MyClass {
+  private something : string;
+  constructor(something: string) {
+    this.something = something;
+  }
+  private doSomething = (input: string, anotherInput: number) : string => {
+  }
+
+}
+
+`
+		assert.Equal(
+			t,
+			out,
+			expected,
+		)
 	})
-
-	out := cg.String()
-	t.Log(out)
-
-	t.Fail()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/adaam2/ts-generator-go
 
 go 1.18
+
+require gotest.tools/v3 v3.3.0
+
+require github.com/google/go-cmp v0.5.5 // indirect
